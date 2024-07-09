e2a4c5271801d26f35cb74640960968fc50d1e57
Konstantin Maurin <artsjesus7@gmail.com>
2024-07-09 23:25:52 +0300

    fix

--- src/vacancies.py
+++ src/vacancies.py
@@ -1,22 +1,25 @@
 class JobVacancy:
-    def __init__(self, name: str, url: str, salary: dict, description: str):
+    def __init__(self, name: str, salary: dict, url: str, requirement: str):
         if not isinstance(salary, dict):
             raise TypeError("Salary must be a dictionary")
         self.name = name
-        self.url = url
         self.salary = salary
-        self.description = description
-
-    def __gt__(self, other):
-        return self.salary['to'] > other.salary['to']
+        self.url = url
+        self.requirement = requirement
 
     def __str__(self):
         return (
             f"Название: {self.name}\n"
-            f"Зарплата: от {self.salary} до {self.salary['to']} {self.salary}\n"
+            f"Зарплата: от {self.salary['from']} до {self.salary['to']} {self.salary['currency']}\n"
             f"Ссылка: {self.url}\n"
-            f"Требования: {self.description}\n"
+            f"Требования: {self.requirement}\n"
         )
 
     def __repr__(self):
-        return f"{JobVacancy.__name__}({self.name}, {self.salary}, {self.url}, {self.description})"
+        return f"Vacancy({self.name}, {self.salary}, {self.url}, {self.requirement})"
+
+    def __gt__(self, other):
+        return self.salary['to'] > other.salary['to']
+
+    def __lt__(self, other):
+        return self.salary['to'] < other.salary['to']
